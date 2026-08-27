9bee8f226a3a27c7299a6c6466e193470ba52948
Nummedal <sigrun.asheim.nummedal@nav.no>
2026-08-27 15:30:49 +0200

    Legg til errorlogs for response og logg request body

--- src/proxy.ts
+++ src/proxy.ts
@@ -70,11 +70,12 @@ proxyApp.all("/:prefix/:path{.*}", async (c) => {
     body: await c.req.blob(),
   });
 
-  teamLogger.debug(
+  teamLogger.info(
     `Outgoing proxy request
      IN: ${c.req.method} ${c.req.url}
      OUT: ${proxyRequest.method} ${proxyRequest.url}
      Headers: ${JSON.stringify(proxyRequest.headers)}
+     Body: ${JSON.stringify(proxyRequest.body)}
      `,
   );
 
@@ -82,11 +83,19 @@ proxyApp.all("/:prefix/:path{.*}", async (c) => {
     redirect: "manual",
   });
 
-  teamLogger.debug(
-    `Proxy response from ${proxyRequest.url}: ${res.status}
+  if (res.status > 199 && res.status < 300) {
+    teamLogger.info(
+      `Proxy response from ${proxyRequest.url}: ${res.status}
      Headers: ${JSON.stringify(res.headers)}
     `,
-  );
+    );
+  } else {
+    teamLogger.error(
+      `Proxy response from ${proxyRequest.url}
+             Status: ${res.status}
+              Headers: ${JSON.stringify(res.headers)}`,
+    );
+  }
 
   // Bun adds its own Date header, so delete any
   // from upstream to not confuse and spam warnings from the loadbalancer.
